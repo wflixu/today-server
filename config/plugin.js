29f2928e4f7c68d06cdd94a9fda4c72f87705016
wflixu <wflixu@88.com>
2021-05-05 20:11:27 +0800

    端口号修改

--- config/plugin.js
+++ config/plugin.js
@@ -1,34 +1,34 @@
-'use strict';
+"use strict";
 
 /** @type Egg.EggPlugin */
 module.exports = {
+
   // had enabled by egg
   static: {
     enable: true,
   },
   mongoose: {
     enable: true,
-    package: 'egg-mongoose',
+    package: "egg-mongoose",
   },
   handlebars: {
     enable: true,
-    package: 'egg-view-handlebars',
+    package: "egg-view-handlebars",
   },
   cors: {
     enable: true,
-    package: 'egg-cors'
+    package: "egg-cors",
   },
   validate: {
     enable: true,
-    package: 'egg-validate',
+    package: "egg-validate",
   },
   bcrypt: {
     enable: true,
-    package: 'egg-bcrypt'
+    package: "egg-bcrypt",
   },
   jwt: {
     enable: true,
-    package: 'egg-jwt',
-  }
+    package: "egg-jwt",
+  },
 };
-
