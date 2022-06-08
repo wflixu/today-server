405403a4986460d7011e4f84bb1543a875fa4a6e
lixu <wflixu@88.com>
2022-06-08 23:30:39 +0800

    更新

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,12 +5,12 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    // ctx.body = 'hello egg.js';
-    await ctx.render('home',{
-      keywords:'react view template',
-      description:'alsdjfkj laksjdkf jaklsdjf ',
-      title:'home'
-    });
+    ctx.body = 'hello egg.js';
+    // await ctx.render('home',{
+    //   keywords:'react view template',
+    //   description:'alsdjfkj laksjdkf jaklsdjf ',
+    //   title:'home'
+    // });
     ctx.status = 200;
   }
 }
