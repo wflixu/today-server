1d06d0949b5814a07f916785aea738727414ac9d
lixu <wflixu@88.com>
2023-04-12 12:44:17 +0800

    chore: 框架由egg切换到midway

--- README.md
+++ README.md
@@ -1,29 +1,16 @@
-# today-server
+# Today-server
 
-基于Egg.js的 RESTful API 服务，用于快速集成开发RESTful前后端分离的服务端。 (建议用于学习入门EGGJS和Mongoose,如果作为生产请自行优化和改造)
-
-前台使用vite TypeScript vue3 ant-design-vue
-
-## 特性
-- ⚡ 框架选择：基于 Egg.js 2.0 
-- 🔥 数据模型：基于 Mongoose 存储
-- 🔒 授权验证：基于JWT
-- 🚀 内置功能：文件处理，用户系统，统一错误处理及接口返回标准，全方位CRUD,分页,模糊查询的等数据操作
 ## QuickStart
 
 <!-- add docs here for user -->
 
-see [egg docs][egg] for more detail.
+see [midway docs][midway] for more detail.
 
-
-## Development
+### Development
 
 ```bash
 $ npm i
-# 启动后台
-$ npm run dev 
-# 启动前台
-$ npm run dev:vite 
+$ npm run dev
 $ open http://localhost:7001/
 ```
 
@@ -31,14 +18,11 @@ $ open http://localhost:7001/
 
 ```bash
 $ npm start
-$ npm stop
 ```
 
 ### npm scripts
 
 - Use `npm run lint` to check code style.
 - Use `npm test` to run unit test.
-- Use `npm run autod` to auto detect dependencies upgrade, see [autod](https://www.npmjs.com/package/autod) for more detail.
 
-
-[egg]: https://eggjs.org
+[midway]: https://midwayjs.org
---
+++ src/configuration.ts
@@ -0,0 +1,37 @@
+import { Configuration, App } from '@midwayjs/core';
+import * as koa from '@midwayjs/koa';
+import * as validate from '@midwayjs/validate';
+import * as info from '@midwayjs/info';
+import { join } from 'path';
+// import { DefaultErrorFilter } from './filter/default.filter';
+// import { NotFoundFilter } from './filter/notfound.filter';
+import { ReportMiddleware } from './middleware/report.middleware';
+import { AuthGuard } from './guard/auth.guard';
+import * as orm from '@midwayjs/typeorm';
+
+@Configuration({
+  imports: [
+    koa,
+    validate,
+    orm,
+    {
+      component: info,
+      enabledEnvironment: ['local'],
+    },
+  ],
+  importConfigs: [join(__dirname, './config')],
+})
+export class ContainerLifeCycle {
+  @App()
+  app: koa.Application;
+
+  async onReady() {
+    // add middleware
+    this.app.useMiddleware([ReportMiddleware]);
+   
+    // add filter
+    // this.app.useFilter([NotFoundFilter, DefaultErrorFilter]);
+    // add Guard
+    this.app.useGuard(AuthGuard)
+  }
+}
