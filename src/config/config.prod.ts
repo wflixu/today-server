32cb510da88034dbac6e071923a0d6d56c300f9c
lixu <wflixu@88.com>
2023-04-13 18:29:55 +0800

    chore: datasource

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -5,4 +5,19 @@ export default {
   koa: {
     port: 8443,
   },
+  typeorm: {
+    dataSource: {
+      default: {
+        type: 'postgres',
+        host: '127.0.0.1',
+        port: 5432,
+        username: 'postgres',
+        password: 'pg88',
+        database: 'pingpong',
+        synchronize: true, // 如果第一次使用，不存在表，有同步的需求可以写 true，注意会丢数据
+        logging: false,
+        entities: ['**/entity/*.entity{.ts,.js}'],
+      },
+    },
+  },
 } as MidwayConfig;
