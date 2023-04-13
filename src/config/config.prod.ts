116bb6e296ef1e29649bbe1df973d8a71ca000fe
lixu <wflixu@88.com>
2023-04-13 12:20:00 +0800

    chore: https

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -1,8 +1,8 @@
 import { MidwayConfig } from '@midwayjs/core';
 
 export default {
-  keys:'1639994056460_8009',
+  keys: '1639994056460_8009',
   koa: {
-    port: 7001,
+    port: 8443,
   },
 } as MidwayConfig;
